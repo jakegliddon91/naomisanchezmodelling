5d53539655b9615030164c05ba8be0bc05da9a90
TheFADco <j.gliddon@thefadco.co.uk>
2018-12-18 01:58:41 +0000

    added header and footer files as well as styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 	root 'pages#index'
-  get 'pages/index'
-  get 'pages/about'
-  get 'pages/portfolio'
-  get 'pages/writing'
-  get 'pages/contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/index',    	to: 'pages#index'
+  get '/about',   	to: 'pages#about'
+  get '/contact', 	to: 'pages#contact'
+  get '/portfolio',	to: 'pages#portfolio'
+  get '/writing',	to: 'pages#writing'
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
