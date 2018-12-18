0e10cd8220613b69c4a595c320d8c98003d04138
TheFADco <j.gliddon@thefadco.co.uk>
2018-12-18 01:20:28 +0000

    routed homepage and created placeholder

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+	root 'pages#index'
+  get 'pages/index'
+  get 'pages/about'
+  get 'pages/portfolio'
+  get 'pages/writing'
+  get 'pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
